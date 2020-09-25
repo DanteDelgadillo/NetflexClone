cb11bbd039bba2d21fa53210da9aff0af515cefc
dante <40077234+DanteDelgadillo@users.noreply.github.com>
2020-09-25 13:54:05 -0700

    update

--- src/request.js
+++ src/request.js
@@ -1,14 +1,15 @@
 
+const React_App_Key = "f1cbea6e7cfcec49777dc013659da0e0"
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`,
-    fetchNetflixOrignals: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_network=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=99`
+    fetchTrending: `/trending/all/week?api_key=${React_App_Key}&language=en-US`,
+    fetchNetflixOrignals: `/discover/tv?api_key=${React_App_Key}&with_network=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${React_App_Key}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${React_App_Key}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${React_App_Key}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${React_App_Key}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${React_App_Key}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${React_App_Key}&with_genres=99`
 
 }
 
